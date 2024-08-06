6ac637dcb9c75aae9b61f58ec51a872b8e95c7fa
Ishan Bagchi <ishanbagchi4@gmail.com>
2024-08-07 02:31:07 +0530

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+	build: {
+		lib: {
+			entry: 'src/index.ts',
+			name: 'MyUILibrary',
+			fileName: (format) => `aynaui.${format}.js`,
+		},
+		rollupOptions: {
+			external: ['react', 'react-dom'],
+			output: {
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM',
+				},
+			},
+		},
+	},
+	plugins: [react(), dts()],
+})
